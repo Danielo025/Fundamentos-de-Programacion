2783d4e6c6c9a3f4e0c4c9391f13b6d05d4e7eba
Daniel <Daniel@DESKTOP-HEGLGG6>
2022-11-04 17:27:45 -0500

    Agregar archivos de proyecto.

---
+++ ex03_VentasBarrioOriginal/Pedido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03_VentasBarrioOriginal
+{
+    public class Pedido
+    {
+        //Atributos
+        private string barrio;
+        private int cantidadGalletas;
+
+        //Constructor
+        public Pedido()
+        {
+            barrio = "";
+            cantidadGalletas = 0;
+        }
+
+        //Propiedades
+        public string Barrio
+        {
+            get { return barrio; }
+            set { barrio = value; }
+        }
+
+        public int CantidadGalletas
+        {
+            get { return cantidadGalletas; }
+            set { cantidadGalletas = value; }
+        }
+    }
+}
